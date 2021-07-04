f97c97e34d9c5da4a5022641048146939f31e988
Lucas-Brum <lucasmatheusrodriguesbrum@gmail.com>
2021-07-04 18:52:54 -0300

    Coloquei codigo em funções e adicionei a detecção de teclas que foram clicadas

--- game.js
+++ game.js
@@ -1,17 +1,39 @@
 function draw() {
-        var canvas = document.getElementById('screen')
-        if (canvas.getContext) {
-          var ctx = canvas.getContext('2d')
-                
-                ctx.fillStyle="#008000"
-                ctx.fillRect(10, 10, 10, 5)
+  var canvas = document.getElementById('screen')
 
-                ctx.fillStyle="#7F7F00";
-                ctx.beginPath();
-                ctx.arc(100,100,5,0,Math.PI*2,true);
-                ctx.closePath();
-                ctx.fill();
-        }
+  function generatesPlayer(){
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d')
+                  
+      ctx.fillStyle="#008000"
+      ctx.fillRect(10, 10, 10, 5)
+
       }
+    }
+        
+  function generatesCoin(){
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d')
+                  
+      ctx.fillStyle="#7F7F00";
+      ctx.beginPath();
+      ctx.arc(100,100,5,0,Math.PI*2,true);
+      ctx.closePath();
+      ctx.fill();
+
+      }
+    }
+
+    const botoes = document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+        return keyName
+      });
+        movePlayer(){
+          
+        }
+        generatesPlayer()
+        generatesCoin()
+        
+}
 
 draw()
